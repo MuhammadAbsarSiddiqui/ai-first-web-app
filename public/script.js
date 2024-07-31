1a484061f205b078302dcc0b01dac77666c2dee1
Anzeela-Fatima <anzila.fatima@gmail.com>
2024-08-01 04:21:44 +0500

    initial commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// // public/script.js
+// $(document).ready(function(){
+//     $('.carousel')({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+// });
